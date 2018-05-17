1edb2a9175b2e5cc2d53f867ecb8bd33be615168
Unknown <benjamin_pestoor@hotmail.com>
2018-05-17 10:31:23 +0200

    added nullglob and finish message

--- fotosort.sh
+++ fotosort.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 echo "script week 2"
 
-dir1=$1
-groep=$2
+cd $1
+dir1="$1"
 
-cd $dir1
-
-for i in *.png *.bmp *.jpeg *.jpg; do
+shopt -s nullglob
+shopt -s nocaseglob
+number=0
+for i in *.jpg; do
 	if [[ $2 = "maand" ]]
 	then
 		date1=$(date -r $i +%B)
-		mkdir -r maand
+		mkdir -p maand
 	elif [[ $2 = "week" ]]
 	then
 		date1=$(date -r $i +%W)
-		mkdir -r week
+		mkdir -p week
 	else
 		echo "second variable must be maand or week"
 	fi
@@ -23,11 +24,20 @@ for i in *.png *.bmp *.jpeg *.jpg; do
 	cp $i ./$2/$date1
 	cd ./$2/$date1
 	md52=$(md5sum $i)
-	cd -
+	cd ~-
 	if [ "$md51" == "$md52" ]
 	then
 		rm -f $i
 	else
 		echo "$1 is not the same"
 	fi
+	((number++))
 done
+if [[ $number < 1 ]]
+then
+	echo "there were no files to sort"
+else
+	echo "$number files were sorted"
+fi
+shopt -u nocaseglob
+shopt -u nullglob
